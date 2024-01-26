1328368d37150438a0033e05af55b4e8956017fa
Usborn Ocampo <usborn116@gmail.com>
2024-01-25 16:47:09 -0800

    added skill

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
       <div className="section">
         <h1 id='skills'>Skills + Knowledge</h1>
         <div className='list-grid'>
-          <Skills type='Coding' names={['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Python', 'Ruby', 'Ruby on Rails',
+          <Skills type='Coding' names={['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Python', 'Django', 'Ruby', 'Ruby on Rails',
           'React', 'Vite', 'SQL', 'R', 'Command Line/Terminal', 'Git', 'Github', 'Browser DevTools', 'Visual Studio Code']}/>
           <Skills type='Software' names={['Windows', 'Mac', 'iOS', 'Tableau', 'Fly.io', 'Netlify', 'Zendesk',
            'Trello', 'Asana', 'Zapier', 'Google Suite', 'Microsoft Suite', 'Datadog']}/>
